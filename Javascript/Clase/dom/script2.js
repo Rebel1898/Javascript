1b521583a05ad53116e1979a355bb09410690313
Rebel1898 <Sergio.llamas@protonmail.ch>
2018-10-26 16:57:09 +0200

    javascript3

---
+++ Javascript/Clase/dom/script2.js
@@ -0,0 +1,12 @@
+let titulo = document.getElementById('cambiar');
+cambiar.onclick=function(){ //Usar boton para cambiar elementos HTML
+
+
+
+
+titulo.innerHTML="perrete, rey del torrent";
+titulo.style.color="red";
+
+let elParr= document.getElementById("pere");
+elParr.innerHTML = "xxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+}
---
+++ Javascript/Clase/dom/script3.js
@@ -0,0 +1,12 @@
+
+
+
+document.getElementById("upper").onclick=function()
+{ 
+    document.getElementById("parrafo1").innerHTML=document.getElementById("parrafo1").innerHTML.toUpperCase();
+
+
+    }
+
+//Para el segundo boton toLowercase
+    //Usar boton para cambiar elementos HTML
