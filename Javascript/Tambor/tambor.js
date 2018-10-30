f79652263345dd4ab280ee5912d42fa3faa6e787
Rebel1898 <Sergio.llamas@protonmail.ch>
2018-10-30 10:43:29 +0100

    t

--- Javascript/Tambor/tambor.js
+++ Javascript/Tambor/tambor.js
@@ -1,37 +1,9 @@
 let notas = [
 
     { nombre: 'do', sonido: './wav/c1.wav', tono: true },
- 
     { nombre: 'dos', sonido: './wav/c1s.wav', tono: false },
- 
     { nombre: 're', sonido: './wav/d1.wav', tono: true },
- 
-    { nombre: 'res', sonido: './wav/d1s.wav', tono: false },
- 
-    { nombre: 'mi', sonido: './wav/e1.wav', tono: true },
- 
-    { nombre: 'fa', sonido: './wav/f1.wav', tono: true },
- 
-    { nombre: 'fas', sonido: './wav/f1s.wav', tono: false },
- 
-    { nombre: 'sol', sonido: './wav/g1.wav', tono: true },
- 
-    { nombre: 'sols', sonido: './wav/g1s.wav', tono: false },
- 
-    { nombre: 'la', sonido: './wav/a1.wav', tono: true },
- 
-    { nombre: 'las', sonido: './wav/a1s.wav', tono: false },
- 
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'si', sonido: './wav/b1.wav', tono: true },
-    { nombre: 'las', sonido: './wav/a1s.wav', tono: false },
 
- 
  ];
  
  
